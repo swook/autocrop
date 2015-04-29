14e43d0df80fb2ea0a093be0a657b33be38245cb
Seon-Wook Park <seon.wook@swook.net>
2015-04-29 23:33:52 +0200

    Move pipeline.sh to scripts/trainer_pipeline.sh

--- scripts/trainer_pipeline.sh
+++ scripts/trainer_pipeline.sh
@@ -6,11 +6,14 @@ cd $(dirname "${BASH_SOURCE[0]}")
 BUILD="../build"
 
 # Preprocess Chen images
+echo; echo "> Preprocessing Chen images..."
 $BUILD/saliency ../datasets/Chen/image --output-dir ../datasets/Chen/image
 
 # Calculate features
+echo; echo "> Calculating features..."
 $BUILD/features
 
 # Run trainer
+echo; echo "> Running trainer..."
 $BUILD/trainer
 
