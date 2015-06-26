6259a6386eb6ae8a477dee08f0de5e8bee26a092
Seon-Wook Park <seon.wook@swook.net>
2015-06-26 02:36:03 +0200

    Add Classifier.py.getScore for visualise.py

--- suitability/Classifier.py
+++ suitability/Classifier.py
@@ -13,6 +13,9 @@ class Classifier:
         feats = FeatMat().getFeature(imgf)
         return self.predictFeats(feats)
 
+    def getScore(self, feats):
+        return self.clf.decision_function(feats)[0][0]
+
     def predictFeats(self, feats):
         feats = self.preprocess.transform(feats)
         return int(self.clf.predict(feats)[0])
