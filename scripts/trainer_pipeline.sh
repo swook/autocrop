0cbf98b2ee59937ee5655fde5395340536a0e499
Seon-Wook Park <seon.wook@swook.net>
2015-07-16 20:57:35 +0900

    Add time to trainer_pipeline.sh

--- scripts/trainer_pipeline.sh
+++ scripts/trainer_pipeline.sh
@@ -7,17 +7,17 @@ BUILD="../build"
 
 # Preprocess Chen images
 echo; echo "> Preprocessing Chen images..."
-$BUILD/saliency ../datasets/Chen/image --output-dir ../datasets/Chen/image
+time $BUILD/saliency ../datasets/Chen/image --output-dir ../datasets/Chen/image
 
 # Preprocess Reddit images
 echo; echo "> Preprocessing Reddit images..."
-$BUILD/saliency ../datasets/Reddit --output-dir ../datasets/Reddit
+time $BUILD/saliency ../datasets/Reddit --output-dir ../datasets/Reddit
 
 # Calculate features
 echo; echo "> Calculating features..."
-$BUILD/features
+time $BUILD/features
 
 # Run trainer
 echo; echo "> Running trainer..."
-$BUILD/trainer
+time $BUILD/trainer
 
